91a8c181baa92d26292c94dd01ac075a43302927
vineet34064 <vineethpolampali@gmail.com>
2026-03-13 11:45:57 +0000

    add mess menu view

---
+++ backend/src/models/Message.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+  read: { type: Boolean, default: false },
+}, { timestamps: true });
+
+export const Message = mongoose.model('Message', messageSchema);
